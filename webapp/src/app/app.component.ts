6c5e83769ef15d91f003637067878526463d8726
VagnerSilva <vagner23silva@gmail.com>
2021-12-06 13:26:32 -0300

    add web project

---
+++ webapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'jgv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'webapp';
+}
