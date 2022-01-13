a36167c12c423382421f44a77575a287e56ad286
jessicapcruz <jessica_p_cruz@hotmail.com>
2022-01-13 11:58:54 -0300

    divisão dos serviços entre cadastro e consulta

--- consulta/src/main/java/com/sociallearnapi/consulta/controller/LearningContentController.java
+++ consulta/src/main/java/com/sociallearnapi/consulta/controller/LearningContentController.java
@@ -23,19 +23,6 @@ public class LearningContentController {
     @Autowired
     LearningContentService learningContentService;
 
-    @ApiOperation(value = "Register learning content")
-    @PostMapping("/register")
-    public ResponseEntity<LearningContentResponse> schedule(
-            @RequestBody LearningContentRequest learningContentRequest) {
-        try {
-            var savedLearningContent = learningContentService.save(learningContentRequest.toEntity());
-            return ResponseEntity.ok().body(savedLearningContent.toResponse());
-        } catch (ApiErrorException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).header(e.getMessage()).build();
-        }
-    }
-
     @ApiOperation(value = "Search learning content by id")
     @GetMapping("/{learningContentId}")
     public ResponseEntity<LearningContentResponse> findById(
@@ -57,43 +44,5 @@ public class LearningContentController {
                 .map(learningContent -> learningContent.toResponse())
                 .collect(Collectors.toList());
         return ResponseEntity.ok().body(learningContentResponseList);
-    }
-
-    @ApiOperation(value = "Update learning content")
-    @PutMapping("/update/{learningContentId}")
-    public ResponseEntity<LearningContentResponse> update(@PathVariable String learningContentId,
-                                                          @RequestBody LearningContentRequest learningContentRequest) {
-        try {
-            var learningContent =
-                    learningContentService.update(learningContentId, learningContentRequest.toEntity());
-            return ResponseEntity.ok().body(learningContent.toResponse());
-        } catch (ApiErrorException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).header(e.getMessage()).build();
-        }
-    }
-
-    @ApiOperation(value = "Inactivate learning content")
-    @PutMapping("/inactivate/{learningContentId}")
-    public ResponseEntity<LearningContentResponse> inactivate(@PathVariable String learningContentId) {
-        try {
-            var learningContent = learningContentService.inactivate(learningContentId);
-            return ResponseEntity.ok().body(learningContent.toResponse());
-        } catch (ApiErrorException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @ApiOperation(value = "Delete learning content")
-    @DeleteMapping("/delete/{learningContentId}")
-    public ResponseEntity<String> delete(@PathVariable String learningContentId) {
-        try {
-            learningContentService.deleteById(learningContentId);
-            return ResponseEntity.ok().body("Learning content deleted");
-        } catch (EmptyResultDataAccessException e) {
-            e.printStackTrace();
-            return ResponseEntity.notFound().build();
-        }
     }
 }
